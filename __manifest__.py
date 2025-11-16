803036167930b90238946a003785425e17c6f531
Antonio Queb <cardstillo@gmail.com>
2025-11-15 22:20:04 -0600

    up

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,15 @@
     'name': 'Product Price Security',
     'version': '19.0.1.0.0',
     'category': 'Product',
+    'author': 'Alphaqueb Consulting SAS',
+    'website': 'https://www.alphaqueb.com',
+    'license': 'AGPL-3',
+    'summary': 'Control access to product sale prices based on user groups.',
+    'description': """
+        This module restricts the visibility and modification of product sale prices
+        to users who belong to a specific group. Unauthorized users will see the
+        sale price as zero.
+    """,
     'depends': ['product', 'stock'],
     'data': [
         'security/product_security.xml',
